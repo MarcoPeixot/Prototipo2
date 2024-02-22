f14a856e3f6a3bdce583fdc755e18f09328960d7
MarcoPeixot <marco.peixoto@sou.inteli.edu.br>
2024-02-21 23:07:52 -0300

    update project

--- mainScene.js
+++ mainScene.js
@@ -1,5 +1,12 @@
 // mainScene.js
-export default class MainScene extends Phaser.Scene{
+import Animacoes from './animation.js'; // Importa um módulo de gerenciamento de animações
+import Player from './player.js'; // Importa a classe Player
+import Camera from './camera.js'; // Importa a classe Camera
+import Controls from './controles.js'; // Importa a classe Controls
+
+var mudarCena = 0; // Variável para controlar a mudança de cena
+
+export default class MainScene extends Phaser.Scene {
     constructor() {
         super({
             key: 'mainScene'
@@ -7,107 +14,76 @@ export default class MainScene extends Phaser.Scene{
     }
 
     preload() {
-        this.load.image("tiles", "./assets/grass.png");
-        this.load.image("objetos", "./assets/object.png");
-        this.load.image("floresta", "./assets/plant.png");
-        this.load.tilemapTiledJSON("mapa", "./assets/map.json");
+        // Carrega os recursos necessários
+        this.load.image("tile_grass", "./assets/novo_mapa/grass.png");
+        this.load.image("tile_water", "./assets/novo_mapa/water.png");
+        this.load.image("tile_objetos", "./assets/novo_mapa/objetos.png");
+        this.load.tilemapTiledJSON("map_florest", "./assets/novo_mapa/new_map.json");
         this.load.spritesheet("tyler", "./assets/novo_persona.png", { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet("old_tyler", "./assets/Persona-principal.png", { frameWidth: 32, frameHeight: 32 });
     }
 
     create() {
-        this.map = this.make.tilemap({key: "mapa"}),
-        this.tilesetGround = this.map.addTilesetImage("grass", "tiles");
-        this.tilesetObject = this.map.addTilesetImage("object", "objetos");
-        this.tilesetFlorest = this.map.addTilesetImage("plant", "floresta");
+        // Cria o mapa e as camadas
+        this.map = this.make.tilemap({ key: "map_florest" });
+        this.tilesetGrass = this.map.addTilesetImage("grass", "tile_grass");
+        this.tilesetWater = this.map.addTilesetImage("water", "tile_water");
+        this.tilesetObject = this.map.addTilesetImage("objetos", "tile_objetos");
 
-        this.ground = this.map.createLayer("grass", this.tilesetGround, 0, 0);
-        this.object = this.map.createLayer("object", this.tilesetObject, 0, 0);
-        this.above = this.map.createLayer("above", this.tilesetFlorest, 0, 0);
+        this.ground = this.map.createLayer("ground", this.tilesetGrass, 0, 0); // cria a layer ground
+        this.water = this.map.createLayer("water", this.tilesetWater, 0, 0); // cria a layer water
+        this.ponte = this.map.createLayer("ponte", this.tilesetObject, 0, 0); // cria a layer ponte
+        this.object = this.map.createLayer("object", this.tilesetObject, 0, 0); // cria a layer object
 
-        this.object.setCollisionByProperty({collider: true}); 
-        this.above.setCollisionByProperty({collider: true})
-        this.above.setDepth(10);
-        this.tyler = this.physics.add.sprite(100, 200, "tyler").setScale(2);
+        this.water.setCollisionByProperty({ collider: true }); //ativa a colisão com a água
+        this.object.setCollisionByProperty({ collider: true }); //ativa a colisão com os objetos
+        this.object.setDepth(10); // faz o persona ficar atrás da layer
+
+        // Encontra o ponto de spawn do NPC no mapa
+        const spawnPointNpc = this.map.findObject(
+            "npc1",
+            (objects) => objects.name === "spawning point npc"
+        );
+
+        // Cria o NPC
+        this.npc = this.physics.add.sprite(spawnPointNpc.x, spawnPointNpc.y, "old_tyler").setScale(2).setFlip(true, false);
+
+        // Verifica o valor de mudarCena e configura a posição inicial do jogador e os controles
+        if (mudarCena === 0) {
+            this.tyler = new Player(this, 100, 400, 'tyler');
+            this.controls = new Controls(this, this.tyler);
+        }
+        if (mudarCena === 1) {
+            this.tyler = new Player(this, 900, 400, 'tyler');
+            this.controls = new Controls(this, this.tyler);
+        }
+
+        // Adiciona colisores entre o jogador e os elementos do mapa
+        this.physics.add.collider(this.tyler, this.water);
         this.physics.add.collider(this.tyler, this.object);
-        this.physics.add.collider(this.tyler, this.above);
 
-        this.anims.create({
-            key: 'descer',
-            frames: this.anims.generateFrameNumbers('tyler', { start: 0, end: 2 }),
-            frameRate: 10,
-            repeat: -1
-        })
+        this.transicaoPonte = 1000; // Define a posição X para a transição de cena
 
-        this.anims.create({
-            key: 'andar_esquerda',
-            frames: this.anims.generateFrameNumbers('tyler', { start: 3, end: 5 }),
-            frameRate: 10,
-            repeat: -1
-        })
+        // Cria as animações utilizando o Animacoes
+        Animacoes.createAnimations(this);
 
-        this.anims.create({
-            key: 'andar_direita',
-            frames: this.anims.generateFrameNumbers('tyler', { start: 6, end: 8 }),
-            frameRate: 10,
-            repeat: -1
-        })
-
-        this.anims.create({
-            key: 'subir',
-            frames: this.anims.generateFrameNumbers('tyler', { start: 9, end: 11 }),
-            frameRate: 10,
-            repeat: -1
-        })
-
-        this.cursor = this.input.keyboard.addKeys({
-            'up': Phaser.Input.Keyboard.KeyCodes.W,
-            'down': Phaser.Input.Keyboard.KeyCodes.S,
-            'right': Phaser.Input.Keyboard.KeyCodes.D,
-            'left': Phaser.Input.Keyboard.KeyCodes.A
-        })
-
-
-
-        this.camera = this.cameras.main
-        this.camera.startFollow(this.tyler);
-        this.camera.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        // Cria a câmera do jogador
+        this.playerCamera = new Camera(this, this.tyler, this.map);
     }
 
     update() {
-        this.moviment()
+        // Atualiza os controles do jogador
+        this.controls.update();
+
+        // Verifica se o jogador atingiu a posição de transição de cena
+        if (this.tyler.x >= this.transicaoPonte) {
+            this.transitionToScene2('scene2');
+            mudarCena = 1; // Atualiza a variável para evitar que a condição seja atendida novamente
+        }
     }
 
-    moviment(){
-        this.tyler.body.setVelocity(0);
-        if (this.cursor.right.isDown) {
-
-            this.tyler.body.setVelocityX(200);
-        } else if (this.cursor.left.isDown) {
-
-            this.tyler.body.setVelocityX(-200);
-        }
-
-        else if (this.cursor.up.isDown) {
-            this.tyler.body.setVelocityY(-200);
-        }
-
-        else if (this.cursor.down.isDown) {
-            this.tyler.body.setVelocityY(200);
-        }
-
-        if (this.tyler.body.velocity.x !== 0 || this.tyler.body.velocity.y !== 0) {
-            if (this.tyler.body.velocity.y < 0) {
-                this.tyler.anims.play('subir', true);
-            } else if (this.tyler.body.velocity.y > 0) {
-                this.tyler.anims.play('descer', true);
-            } else if (this.tyler.body.velocity.x < 0) {
-                this.tyler.anims.play('andar_esquerda', true);
-            } else if (this.tyler.body.velocity.x > 0) {
-                this.tyler.anims.play('andar_direita', true);
-            }
-        }
-        else {
-            this.tyler.anims.stop();
-        }
+    // Método para transição para a cena 2
+    transitionToScene2(cena) {
+        this.scene.start(cena); // Inicia a cena 2
     }
 }
--- secondScene.js
+++ secondScene.js
@@ -1,19 +1,74 @@
-export default class Scene2 extends Phaser.Scene{
+// Importa os módulos necessários
+import Animacao from './animation.js';
+import Player from './player.js';
+import Camera from './camera.js';
+import Controls from './controles.js';
+
+// Define e exporta a classe Scene2
+export default class Scene2 extends Phaser.Scene {
     constructor() {
         super({
             key: 'scene2'
         });
     }
 
+    // Pré-carrega os recursos necessários
     preload() {
-
+        this.load.image("tiles", "./assets/mapa2/samplemap.png");
+        this.load.tilemapTiledJSON("map_praca", "./assets/mapa2/map2.json");
+        this.load.spritesheet("tyler", "./assets/novo_persona.png", { frameWidth: 32, frameHeight: 32 });
     }
 
+    // Cria os elementos do jogo
     create() {
+        // Cria o mapa e define o tileset
+        this.map = this.make.tilemap({ key: "map_praca" });
+        this.tileset = this.map.addTilesetImage("assets", "tiles");
 
+        // Cria as camadas do mapa
+        this.water = this.map.createLayer("water", this.tileset, 0, 0);
+        this.ground = this.map.createLayer("ground", this.tileset, 0, 0);
+
+        // Define colisões com base nas propriedades do mapa
+        this.ground.setCollisionByProperty({ collider: true })
+
+        // Encontra o ponto de spawn do jogador no mapa
+        const spawnPoint = this.map.findObject(
+            "player",
+            (objects) => objects.name === "spawning point"
+        );
+
+        // Cria o jogador, câmera e controles
+        this.tyler = new Player(this, spawnPoint.x, spawnPoint.y, 'tyler');
+        this.camera = new Camera(this, this.tyler, this.map);
+        this.control = new Controls(this, this.tyler);
+
+        // Adiciona colisor entre o jogador e o chão
+        this.physics.add.collider(this.tyler, this.ground);
+
+        // Cria as animações utilizando o Animacao
+        Animacao.createAnimations(this);
+
+        // Cria a câmera do jogador
+        this.playerCamera = new Camera(this, this.tyler, this.map);
+
+        // Define a posição limite para a transição de cena
+        this.passarPonte = 30;
     }
 
+    // Atualiza o jogo
     update() {
+        // Atualiza os controles do jogador
+        this.control.update();
 
+        // Verifica se o jogador atingiu a posição de transição de cena
+        if (this.tyler.x <= this.passarPonte) {
+            this.transitionToScene1("mainScene")
+        }
+    }
+
+    // Método para transição para a cena 1
+    transitionToScene1(cena) {
+        this.scene.start(cena); // Inicia a cena 1
     }
 }
